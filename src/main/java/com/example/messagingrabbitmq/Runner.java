9678eb25a187482f06e93bedd225d09de1ca08f6
Fofan228 <vovka.322@Mail.ru>
2022-12-25 18:57:45 +0500

    1

--- src/main/java/com/example/messagingrabbitmq/Runner.java
+++ src/main/java/com/example/messagingrabbitmq/Runner.java
@@ -1,5 +1,6 @@
 package com.example.messagingrabbitmq;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -20,8 +21,14 @@ public class Runner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Sending message...");
-		rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
-		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		Scanner in = new Scanner(System.in);
+
+		while(true){
+			String message = in.nextLine();
+			rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.exchangeName,
+					MessagingRabbitmqApplication.queueName, message);
+			receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		}
 	}
 
 }
